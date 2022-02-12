3783248a0b514b3b1e3b6c5aca657c2f2eae44ae
daveT83 <40746034+daveT83@users.noreply.github.com>
2022-02-11 21:27:47 -0500

    Initially Example Upload

---
+++ Model Loader Examples/Model Loader Examples/Examples/Example.cs
@@ -0,0 +1,49 @@
+﻿using Model_Loader_Examples.Models;
+using Model_Loader.Infrastructure;
+using System.Collections.Generic;
+
+namespace Model_Loader_Examples.Examples
+{
+    public class Example
+    {
+        public static ExampleModel LoadFromArguemnts(string[] args, string delimeter)
+        {
+            //Loads the arguements with the associated variable name into a dictionary<string,string>
+            bool isTrim = true; //use this if you do not want to trim the values(false) read in from the arguements. By defualt this will trim the values(true).
+            Dictionary<string, string> arguementDictionary = DictionaryCreator.CreateFromArguments(args, delimeter, isTrim);
+
+            //Load the values into the model
+            TypeConverter typeConverter = new TypeConverter();  //you can extend this, but we only need the built in c# types
+                                                                //if you're unsure what types are included in this, please refer to /Models/ExampleModel.cs
+            ModelLoader modelLoader = new ModelLoader(typeof(ExampleModel), typeConverter); //typeof(ExampleModel) gets the type of the model we want to load the values into.
+            ExampleModel model = modelLoader.LoadModel(arguementDictionary);    //Loads the values into the model
+            return model;
+        }
+
+        public static List<ExampleModel> LoadFromFile(string filePath, char delimeter)
+        {
+            //Loads the arguements with the associated variable name into a List<Dictionary<string,string>>
+            //Each dictionary in this list represents a single line of the file.
+            bool isTrim = true; //use this if you do not want to trim the values(false) read in from the arguements. By defualt this will trim the values(true).
+            List<Dictionary<string, string>> arguementDictionaries = DictionaryCreator.CreateFromFlatFile(filePath, delimeter, isTrim);
+
+            //Load the values into the model
+            TypeConverter typeConverter = new TypeConverter();  //you can extend this, but we only need the built in c# types
+                                                                //if you're unsure what types are included in this, please refer to /Models/ExampleModel.cs
+            ModelLoader modelLoader = new ModelLoader(typeof(ExampleModel), typeConverter); //typeof(ExampleModel) gets the type of the model we want to load the values into.
+            List<ExampleModel> models = new List<ExampleModel>();
+
+            foreach (Dictionary<string, string> line in arguementDictionaries)
+            {
+                ExampleModel model = modelLoader.LoadModel(line);   //Loads the values into a model
+                models.Add(model);  //adds the models to the model list.
+                                    //This is a representation of the file after the values have been loaded into several models.
+                                    //Each model is equivilant to one line in the file.
+            }
+
+            return models;
+
+
+        }
+    }
+}
---
+++ Model Loader Examples/Model Loader Examples/Models/ExampleModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model_Loader_Examples.Models
+{
+    public class ExampleModel
+    {
+        public int IntExample { get; set; }
+        public string StringExample { get; set; }
+        public bool BoolExample { get; set; }
+        public char[] CharArrayExample { get; set; }
+        public char CharExample { get; set; }
+        public decimal DecimalExample { get; set; }
+        public double DoubleExample { get; set; }
+        public float FloatExample { get; set; }
+        public uint UIntExample { get; set; }
+        public long LongExample { get; set; }
+        public ulong ULongExample { get; set; }
+        public short ShortExample { get; set; }
+        public ushort UShortExample { get; set; }
+    }
+}
